b4fd441f99af6eee70e4783f37bfbed11f577ec0
kattyaso <sandriver0909@yahoo.co.jp>
2024-01-07 20:32:03 +0900

    テストコード　修正
    パスワードは半角英数字混合の入力が必須であること

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,11 +36,22 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
     end
-    it 'パスワードは、半角英数字混合での入力が必須であること' do
+    it 'パスワードは、半角数字のみでは登録できないこと' do
       @user.password = '123456'
       @user.valid?
       expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
+    it 'パスワードは、半角英字のみでは登録できないこと'do
+    @user.password = 'aaaaaa'
+    @user.valid?
+    expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+    it 'パスワードは、全角文字を含むと登録できないこと'do
+    @user.password = '1a1a1aあ'
+    @user.valid?
+    expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+
     it 'パスワードとパスワード（確認）が一致しない場合は登録できないこと' do
       @user.password = 'password123'
       @user.password_confirmation = 'differentpassword'
