1944af7d7c98c7e7c6c312af99307d80c8be1484
kattyaso <sandriver0909@yahoo.co.jp>
2024-01-07 17:25:19 +0900

    Factorybotの実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nick_name              {'test'}
+    email                 {'test@example'}
+    password              {'000000'}
+    password_confirmation {password}
+    last_name             {'テスト'}
+    first_name            {'テスト'}
+    last_name_kana        {'テスト'}
+    first_name_kana       {'テスト'}
+    birth_date            {Date.today}
+  end
+end
