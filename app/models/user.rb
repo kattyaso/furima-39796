6f7485c8f21b2e09e4a965996b217de70982fe67
kattyaso <sandriver0909@yahoo.co.jp>
2024-01-08 08:35:49 +0900

    正規表現の修正
    カタカナのみへ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     validates :last_name, presence: true
     validates :first_name, presence: true
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ一-龥々ー]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
     validates :last_name_kana, presence: true
     validates :first_name_kana, presence: true
   end
