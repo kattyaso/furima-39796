38c6242aa687bb8c475f04789b5e1f8887a253f2
kattyaso <sandriver0909@yahoo.co.jp>
2024-01-07 17:49:22 +0900

    テストコード実装　新規登録/ユーザー情報

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  describe 'ユーザー新規登録'do
+    it 'nicknameが必須であること'do
+      @user.nick_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nick name can't be blank"
+    end
+    it 'メールアドレスが必須であること'do
+    @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'メールアドレスが一意性であること' do
+      user1 = FactoryBot.create(:user, email: 'test@example.com')
+      user2 = FactoryBot.build(:user, email: 'test@example.com')
+      user2.valid?
+      expect(user2.errors.full_messages).to include('Email has already been taken')
+    end
+    it "メールアドレスは、@を含む必要があること" do
+      user = FactoryBot.build(:user, email: "testexample.com")
+      user.valid?
+      expect(user.errors[:email]).to include("is invalid")
+    end
+    it 'パスワードが必須であること'do
+    @user.password = ''
+    @user.valid?
+    expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+    it "パスワードは、6文字以上での入力が必須であること"do
+      @user.password = "1"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+    end
+    it "パスワードは、半角英数字混合での入力が必須であること"do
+      @user.password ="123456"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+    it 'パスワードとパスワード（確認）が一致しない場合は登録できないこと' do
+      @user.password = 'password123'
+      @user.password_confirmation = 'differentpassword'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+  end
 end
-
-RSpec.describe User
