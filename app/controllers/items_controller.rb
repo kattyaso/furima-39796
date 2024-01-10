88b08b1ca0c2c37c98e13a83187402c775c884ea
kattyaso <sandriver0909@yahoo.co.jp>
2024-01-10 20:51:12 +0900

    ActhiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,38 @@
 class ItemsController < ApplicationController
   def index
+    
   end
+  
+  def new
+    @item = Item.new
+    @categories = Category.all
+    @sales_status =Sales_Status.all
+    @fee_status =Fee_Status.all
+    @origin_address= Originaddress.all
+    @schedule_delivery =Schedule_delivery.all
+  end
+  def create
+    @item = Item.new(item_params)
+    
+
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:item, :description, :category_id, :sales_status_id,:fee_status_id,
+    :origin_address_id, :schedule_delivery_id, :price, :image ).merge(user_id: current_user.id)
+  end
+  def set_item
+    @item=item.find(params[:id])
+  end
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
+
+
+
+
+
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
-  belongs_to ::user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :category_id,numericality:{other_than: 1 , message: "can't be blank"}
+
+  belongs_to :sales_status
+  validates :sales_status_id,numericality:{other_than: 1 , message: "can't be blank"}
+
+  belongs_to :fee_status
+  validates :fee_status_id,numericality:{other_than: 1 , message: "can't be blank"}
+
+  belongs_to :origin_address
+  validates :origin_address_id,numericality:{other_than: 1 , message: "can't be blank"}
+
+  belongs_to :schedule_delivery
+  validates :schedule_delivery_id,numericality:{other_than: 1 , message: "can't be blank"}
+
+  belongs_to :user
   has_one_attached :image
 end
